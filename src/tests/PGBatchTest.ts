62230ba76236a2ecc62c8643754443808182c3a2
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2017-11-04 11:38:13 +0000

    proper use of pg-promise
    This is how you do it when going for the best performance.

--- src/tests/PGBatchTest.ts
+++ src/tests/PGBatchTest.ts
@@ -1,51 +1,50 @@
-import { UsageStats } from "../UsageStats";
+import {UsageStats} from "../UsageStats";
 
-const pgp = require('pg-promise')({ noWarnings: true});
+const pgp = require('pg-promise')({noWarnings: true});
 import {Document} from "../Document";
+
+const cs = new pgp.helpers.ColumnSet([
+    'id',
+    'docId',
+    'label',
+    'context',
+    'distributions:json',
+    'date'
+], {table: 'document'});
 
 export class PGBatchTest {
 
-	public static start(docs: Document[]) {
-		let db = pgp({
-			host: "localhost",
-			port: 5432,
-			user: "postgres",
-			password: "test",
-			database: "perf_test"
-		});
+    public static start(docs: Document[]) {
+        let db = pgp({
+            host: "localhost",
+            port: 5432,
+            user: "postgres",
+            password: "test",
+            database: "perf_test"
+        });
 
-		let start = new Date().getTime();
+        let start = new Date().getTime();
         const us = new UsageStats();
         us.start();
 
+        const sql = pgp.helpers.insert(docs, cs) + ` ON CONFLICT (id) DO UPDATE SET
+                "docsId" = excluded."docId",
+                label = excluded.label,
+                context = excluded.context,
+                distributions = excluded.distributions,
+                date = excluded.date`;
 
-		return db.tx(t => {
-			let inserts: Promise<void>[] = [];
-
-			docs.forEach(doc => {
-				inserts.push(t.none(`insert into document(id, \"docId\", label, context, distributions, date)
-							values($1, $2, $3, $4, $5, $6) ON CONFLICT (id) DO UPDATE SET 
-								\"docId\" =  $2,
-								label = $3,
-								context = $4,
-								distributions = $5,
-								date = $6`,
-					[doc.id, doc.docId, doc.label, doc.context, JSON.stringify(doc.distributions), doc.date]))
-			});
-
-			return t.batch(inserts);
-		}).then(() => {
-			let end = new Date().getTime();
-            const stats = us.stop();
-
-
-            console.log("[PG-Batch] Call to persist took " + (end - start) + " milliseconds.");
-            // console.log(`
-			// 	avg cpu: ${stats.avgCpu}
-			// 	avg memory: ${stats.avgMemory}
-			// `);
-
-            pgp.end();
-		})
-	}
+        return db.none(sql)
+            .then(() => {
+                let end = new Date().getTime();
+                const stats = us.stop();
+                
+                console.log("[PG-Batch] Call to persist took " + (end - start) + " milliseconds.");
+                // console.log(`
+                // 	avg cpu: ${stats.avgCpu}
+                // 	avg memory: ${stats.avgMemory}
+                // `);
+                pgp.end();
+            })
+    }
 }
